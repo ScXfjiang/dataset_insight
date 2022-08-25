9ddf8e24ee979c97b36d0477226a403128231b42
scxfjiang <sc.xfjiang@gmail.com>
2022-08-25 14:08:41 +0100

    update

--- rs/amazon.py
+++ rs/amazon.py
@@ -1,4 +1,6 @@
 import json
+import numpy as np
+import pandas as pd
 
 
 class AmazonDatasetIf(object):
@@ -6,57 +8,85 @@ class AmazonDatasetIf(object):
     http://jmcauley.ucsd.edu/data/amazon/links.html
     """
 
-    def __init__(self, file_path):
-        self.user_id_list = []
-        self.item_id_list = []
-        self.text_review_list = []
-        self.rating_list = []
-        self.user_ids = set()
-        self.item_ids = set()
-        with open(file_path, "rb") as f:
-            for line in f:
+    def __init__(self, path):
+        with open(path, "rb") as f:
+            self.data = pd.DataFrame(
+                index=np.arange(0, len(f.readlines())),
+                columns=["user_id", "item_id", "rating", "text_review"],
+            )
+            f.seek(0)
+            for idx, line in enumerate(f):
                 js = json.loads(line)
-                user_id = str(js["reviewerID"])
-                item_id = str(js["asin"])
-                self.user_ids.add(user_id)
-                self.item_ids.add(item_id)
-                self.user_id_list.append(user_id)
-                self.item_id_list.append(item_id)
-                self.text_review_list.append(str(js["reviewText"]))
-                self.rating_list.append(float(js["overall"]))
+                self.data.loc[idx] = [
+                    str(js["reviewerID"]),
+                    str(js["asin"]),
+                    float(js["overall"]),
+                    str(js["reviewText"]),
+                ]
+            self.user_ids = set(self.data["user_id"])
+            self.item_ids = set(self.data["item_id"])
+            self.item_id2doc = {}
+            for item_id, group in self.data.groupby("item_id"):
+                doc = list(group["text_review"])
+                self.item_id2doc[item_id] = doc
+            self.user_id2doc = {}
+            for user_id, group in self.data.groupby("user_id"):
+                doc = list(group["text_review"])
+                self.user_id2doc[user_id] = doc
 
 
 class AmazonElectronics(AmazonDatasetIf):
-    def __init__(self, file_path):
-        super(AmazonElectronics, self).__init__(file_path)
+    def __init__(self, path):
+        super(AmazonElectronics, self).__init__(path)
         print("number of user: {}".format(len(self.user_ids)))  # 192403
         print("number of item: {}".format(len(self.item_ids)))  # 63001
-        print("number of rating: {}".format(len(self.rating_list)))  # 1689188
 
 
 class AmazonVideoGames(AmazonDatasetIf):
-    def __init__(self, file_path):
-        super(AmazonVideoGames, self).__init__(file_path)
+    def __init__(self, path):
+        super(AmazonVideoGames, self).__init__(path)
         print("number of user: {}".format(len(self.user_ids)))  # 24303
         print("number of item: {}".format(len(self.item_ids)))  # 10672
-        print("number of rating: {}".format(len(self.rating_list)))  # 231780
 
 
 class AmazonGourmetFoods(AmazonDatasetIf):
-    def __init__(self, file_path):
-        super(AmazonGourmetFoods, self).__init__(file_path)
-        print("number of user: {}".format(len(self.user_ids)))  # 14681
-        print("number of item: {}".format(len(self.item_ids)))  # 8713
-        print("number of rating: {}".format(len(self.rating_list)))  # 151254
+    def __init__(self, path):
+        super(AmazonGourmetFoods, self).__init__(path)
+
+        print("*" * 20 + "Basic Statistics" + "*" * 20)
+        num_user = len(self.user_ids)
+        print("number of user: {}".format(num_user))  # 14681
+        num_item = len(self.item_ids)
+        print("number of item: {}".format(num_item))  # 8713
+        num_rating = len(self.data)
+        print("number of rating: {}".format(num_rating))  # 151254
+
+        text_review = self.data["text_review"]
+        print(type(text_review))
+        
+
+        print("*" * 20 + "Rating(Review) Statistics w.r.t. item" + "*" * 20)
+        print(
+            "average num of rating w.r.t. item: {}".format(
+                float(float(num_rating) / num_item)
+            )  # 17.4
+        )
+
+        print("*" * 20 + "Rating(Review) Statistics w.r.t. user" + "*" * 20)
+        print(
+            "average num of rating w.r.t. user: {}".format(
+                float(float(num_rating) / num_user)
+            )  # 10.3
+        )
 
 
 if __name__ == "__main__":
-    amazon_electronics_dataset = AmazonElectronics(
-        "/Users/xfjiang/workspace/dataset/reviews_Electronics_5.json"
-    )
-    amazon_video_games_dataset = AmazonVideoGames(
-        "/Users/xfjiang/workspace/dataset/reviews_Video_Games_5.json"
-    )
+    # amazon_electronics_dataset = AmazonElectronics(
+    #     "/home/people/22200056/workspace/dataset/reviews_Electronics_5.json"
+    # )
+    # amazon_video_games_dataset = AmazonVideoGames(
+    #     "/home/people/22200056/workspace/dataset/reviews_Video_Games_5.json"
+    # )
     amazon_video_games_dataset = AmazonGourmetFoods(
-        "/Users/xfjiang/workspace/dataset/reviews_Grocery_and_Gourmet_Food_5.json"
+        "/home/people/22200056/workspace/dataset/reviews_Grocery_and_Gourmet_Food_5.json"
     )
